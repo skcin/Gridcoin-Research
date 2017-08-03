17235a8841f27897ec6131cb8ac7a1c7f7bb65bc
TheCharlatan <seb.kung@gmail.com>
2017-08-03 21:00:52 +0200

    Fix qt5 compatibility issues

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -19,6 +19,7 @@
 #include <QFileDialog>
 #include <QDesktopServices>
 #include <QThread>
+#include <QUrlQuery> // For QUrl::queryItems
 
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/fstream.hpp>
